28592fd57247191a5baad57252a84c51c40c0ccb
ashirvad satapathy <ashirvad.satapathy01@gmail.com>
2021-11-17 18:42:19 +0530

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function (eleventyConfig) {
+  
+    // You can return your Config object (optional).
+    return {
+      dir: {
+        input: 'src',
+        output: "dist"
+      },
+      templateFormats: [
+        'html',
+        'md',
+        'njk'
+      ],
+      passthroughFileCopy: true
+    }
+}
